f6626f0cbf51f7c962129617716002b23b81833c
Ashary Tamano <ashtamano@yahoo.com>
2026-01-21 19:36:27 +0800

    Update: successful build

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,19 @@ namespace PrayerTimes
 {
     public partial class MainWindow : Window
     {
+        private void ScrollToMonthlyPrint_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Works even when the named element is inside a different namescope/template
+                var target = LogicalTreeHelper.FindLogicalNode(this, "MonthlyPrintSection") as FrameworkElement;
+                target?.BringIntoView();
+            }
+            catch
+            {
+                // no-op
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
